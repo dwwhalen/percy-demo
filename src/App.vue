ba8c84e4717b14c68ba9d592e4d712be59cf5d15
Dennis Whalen <dwwhalen@gmail.com>
2022-05-22 09:36:44 -0400

    more github integration testing

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it! But you just got lucky." />
+      <HelloWorld msg="You did it! But you just got lucky yo." />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
