f9c2bad9b1e631acea04f004f1d7f769cc5d765c
Dennis Whalen <dwwhalen@users.noreply.github.com>
2022-05-29 09:24:56 -0400

    Add percy (#4)
    * added testing for about page

--- cypress/integration/example.spec.ts
+++ cypress/integration/example.spec.ts
@@ -1,9 +1,21 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe("My First Test", () => {
-  it("visits the app root url", () => {
+  it("visits the home page and about page", () => {
+    //home page
     cy.visit("/");
-    cy.contains("h1", "You did it!");
+    cy.location("pathname").should("eq", "/");
+    cy.get("[data-testid=greeting]").should("contain.text", "You did it");
     cy.percySnapshot("Home Page");
+
+    // about page
+    cy.contains("About").click();
+    cy.location("pathname").should("eq", "/about");
+    cy.get("[data-testid=about-content]").should(
+      "contain.text",
+      "This is an about page"
+    );
+    cy.location("pathname").should("eq", "/about");
+    cy.percySnapshot("About Page");
   });
 });
