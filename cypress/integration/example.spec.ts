76739455fe4b86206598f51806afef1c2d227bbe
Dennis Whalen <dwwhalen@gmail.com>
2022-05-30 09:36:18 -0400

    updated cypress version and simplified funtional test

--- cypress/integration/example.spec.ts
+++ cypress/integration/example.spec.ts
@@ -1,21 +1,17 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
+describe("My only E2E test", () => {
   it("visits the home page and about page", () => {
     //home page
     cy.visit("/");
     cy.location("pathname").should("eq", "/");
-    cy.get("[data-testid=greeting]").should("contain.text", "You did it");
+    // cy.get("[data-testid=greeting]").should("contain.text", "You did it");
     cy.percySnapshot("Home Page");
 
     // about page
     cy.contains("About").click();
     cy.location("pathname").should("eq", "/about");
-    cy.get("[data-testid=about-content]").should(
-      "contain.text",
-      "This is an about page"
-    );
-    cy.location("pathname").should("eq", "/about");
+    // cy.get("[data-testid=about-content]").should("contain.text","This is an about page");
     cy.percySnapshot("About Page");
   });
 });
